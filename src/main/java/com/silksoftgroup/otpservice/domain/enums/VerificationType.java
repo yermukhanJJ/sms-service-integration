fbd2d40ce9a435429141e6714ecf29bc3bb518c7
yermukhanJJ <ermuhannurkali@gmail.com>
2023-08-09 17:09:13 +0600

    Verification type enum

---
+++ src/main/java/com/silksoftgroup/otpservice/domain/enums/VerificationType.java
@@ -0,0 +1,12 @@
+package com.silksoftgroup.otpservice.domain.enums;
+
+public enum VerificationType {
+    WHATSAPP,
+    SMS,
+    VOICE;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
